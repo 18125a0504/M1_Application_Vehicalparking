c6665dbb1f4f6c2af19dbcfaa7e79b8c31e94d41
18125a0504 <94377109+18125a0504@users.noreply.github.com>
2021-11-24 06:53:57 +0530

    Create test_search.h

---
+++ 3_Implementation/test/test_search.h
@@ -0,0 +1,23 @@
+
+#ifndef __TEST_SEARCH_H_
+#define __TEST_SEARCH_H_
+
+void test_checkvehicle(void);
+void test_checkvehno(void);
+
+void test_search_by_vehicle(void);
+void test_update(void);
+
+
+void test_newatt(void);
+void test_newlea(void);
+
+
+void test_avehicle(void);
+void test_tvehical(void);
+void test_spark(void);
+void test_cpark(void);
+
+int test_main(void);
+
+#endif
